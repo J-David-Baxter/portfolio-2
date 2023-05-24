5a847155aece405d83e2bd6d93f53cdcfd4dbbf5
J-David-Baxter <jdavidbx@gmail.com>
2023-05-24 17:58:22 -0400

    Make projects linkable

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   const projects = await getProjects()
@@ -8,24 +9,30 @@ export default async function Home() {
     <div className="max-w-5xl mx-auto py-20">
       <h1 className="text-7xl font-bold">Hello I&apos;m <span className="bg-blue-600 bg-clip-text text-transparent">David</span></h1>
 
-      <p className="mt-3 text-xl text-gray-600">Welcome to my website</p>
+      <p className="mt-3 text-xl text-gray-600">I&apos;m passionate about developing front end applications with modern technologies.</p>
 
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My projects</h2>
 
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {projects.map((project) => (
-          <div key={project._id} className="border border-gray-500 rounded-lg p-3">
+          <Link 
+            key={project._id} 
+            className="border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-600 transition"
+            href={`/projects/${project.slug}`}
+            >
             {project.image && (
               <Image
                 src={project.image}
                 alt={project.name}
-                width={250}
-                height={100}
+                width={750}
+                height={300}
                 className="object-cover rounded-lg border border-gray-500"
               />
             )}
-            <div className="bg-blue-700 bg-clip-text text-transparent">{project.name}</div>
-          </div>
+            <div className="mt-2 bg-blue-600 bg-clip-text text-transparent">
+              {project.name}
+            </div>
+          </Link>
         ))}
       </div>
     </div>
