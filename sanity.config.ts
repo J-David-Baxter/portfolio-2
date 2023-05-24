11caadc93428b7dd57ed778806115ee653c47089
J-David-Baxter <jdavidbx@gmail.com>
2023-05-24 11:29:46 -0400

    Create project schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
+import schemas from './sanity/schemas';
 
 const config = defineConfig({
     projectId: 'g2wi3s2l',
     dataset: 'production',
-    title: 'My Portfolio Website',
+    title: 'David Baxter',
     apiVersion: '2023-05-23',
     basePath: '/admin',
-    plugins: [deskTool()]
+    plugins: [deskTool()],
+    schema: { types: schemas }
 });
 
 export default config;
